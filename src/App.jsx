4095ff6adc1e42aed7825f8a06108f51b353abeb
Giovanny Vargas <yovaz05@gmail.com>
2023-11-08 19:23:13 -0500

    clase 08-11-2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { Container, Grid, Typography } from "@mui/material";
 import { ArticlesProvider } from "./context/ArticlesProvider";
+import FormCategory from "./components/FormCategory";
 
 const App = () => {
   return (
     <>
       <ArticlesProvider>
-        <p>Hola</p>
+        <Container maxWidth="sm">
+          <header>
+            <Typography variant="h3" align="center" marginY={5}>
+              Search News
+            </Typography>
+
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <FormCategory />
+              </Grid>
+            </Grid>
+          </header>
+        </Container>
       </ArticlesProvider>
     </>
   );
