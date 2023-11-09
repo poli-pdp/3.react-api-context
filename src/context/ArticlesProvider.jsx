863d4a108d108a253b2bdf60add8385b70952b30
Giovanny Vargas <yovaz05@gmail.com>
2023-11-08 19:03:53 -0500

    clase 08-11-2023

--- src/context/ArticlesProvider.jsx
+++ src/context/ArticlesProvider.jsx
@@ -11,8 +11,8 @@ const ArticlesProvider = ({ children }) => {
   useEffect(() => {
     const queryAPI = async () => {
       const url = `https://newsapi.org/v2/top-headlines?country=co&category=${category}&apiKey=c105012511a84a1897f5b95b3840de26`;
-      const data = await axios(url);
-      console.log(data);
+      const { data } = await axios(url);
+      setArticles(data.articles);
     };
     queryAPI();
   }, [category]);
